a7c4e8d8a74edf4ea9af099514365712d6b139b9
Maria Pia Sanchez <mapi@platan.us>
2022-03-29 18:31:31 -0300

    feat(fighter-list): add fragment and layout

---
+++ app/src/main/java/cl/sangut/heroesfightapp/views/FighterListFragment.kt
@@ -0,0 +1,44 @@
+package cl.sangut.heroesfightapp.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import cl.sangut.heroesfightapp.R
+import cl.sangut.heroesfightapp.databinding.FragmentFighterListBinding
+
+class FighterListFragment : Fragment() {
+
+  private var _binding: FragmentFighterListBinding? = null
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    _binding = FragmentFighterListBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    addFighterItem()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    _binding = null
+  }
+
+  private fun addFighterItem() {
+    val fighterItemAdapter = FighterItemAdapter()
+
+    activity
+      ?.supportFragmentManager
+      ?.beginTransaction()
+      ?.replace(R.id.heroListView, fighterItemAdapter)
+      ?.commit()
+  }
+}
